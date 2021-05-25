324cef3b03dd02d1f5a6e2308fdd50773e0503ff
Suraj Aravind B <sbollapr@gitam.edu>
2021-05-25 14:48:07 +0530

    added quicksort algorithm to the program

--- 15_sorting_techniques.py
+++ 15_sorting_techniques.py
@@ -51,56 +51,33 @@ def bubble_sort(list_of_numbers):
 
 
 # Python program for implementation of Quicksort Sort
-
-def partition(arr, low, high):
-	i = (low-1)		 # index of smaller element
-	pivot = arr[high]	 # pivot
-
-	for j in range(low, high):
-
-		# If current element is smaller than or
-		# equal to pivot
-		if arr[j] <= pivot:
-
-			# increment index of smaller element
-			i = i+1
-			arr[i], arr[j] = arr[j], arr[i]
-
-	arr[i+1], arr[high] = arr[high], arr[i+1]
-	return (i+1)
+def partition(list_of_numbers, low, high):
+    i = low	 # index of smaller element
+    pivot = list_of_numbers[high]	 # pivot
+    for j in range(low, high):
+        if list_of_numbers[j] <= pivot:
+            list_of_numbers[i], list_of_numbers[j] = list_of_numbers[j], list_of_numbers[i]
+            i = i+1
+    list_of_numbers[i], list_of_numbers[high] = list_of_numbers[high], list_of_numbers[i]
+    return i
 
 
-##def quick_sort(list_of_numbers):
-##	if len(list_of_numbers) == 1:
-##		return list_of_numbers
-##	else:
-##            low = 0
-##            high = len(list_of_numbers - 1)
-##            if low < high:
-##
-##		# pi is partitioning index, arr[p] is now
-##		# at right place
-##		pivot_element = partition(list_of_numbers, low, high)
-##
-##		# Separately sort elements before
-##		# partition and after partition
-##		quickSort(arr, low, pi-1)
-##		quickSort(arr, pi+1, high)
-##
-##
-##n = len(arr)
-##quickSort(arr, 0, n-1)
-##print("Sorted array is:")
-##for i in range(n):
-##	print("%d" % arr[i]),
+def quick_sort(list_of_numbers, low, high):
+    
+    print(list_of_numbers)
+    if low < high:
+        partition_index = partition(list_of_numbers, low, high)
+        quick_sort(list_of_numbers, low, partition_index - 1)
+        quick_sort(list_of_numbers, partition_index + 1, high)
 
 
 list_of_numbers = [19, 1, 11, 13, 5, 6, 9]
 
 ##insertion_sort(list_of_numbers)
 ##selection_sort(list_of_numbers)
-bubble_sort(list_of_numbers)
-##quick_sort(list_of_numbers)
+##bubble_sort(list_of_numbers)
+quick_sort(list_of_numbers, 0, len(list_of_numbers)-1)
+
 
 
 
@@ -125,4 +102,25 @@ print ("Elements after sorting: ", list_of_numbers)
 ##Iteration -  4 Input list =  [1, 5, 6, 9, 19, 11, 13]
 ##Iteration -  5 Input list =  [1, 5, 6, 9, 11, 19, 13]
 ##Iteration -  6 Input list =  [1, 5, 6, 9, 11, 13, 19]
+##Elements after sorting:  [1, 5, 6, 9, 11, 13, 19]
+
+##Output of Bubble Sort
+##Iteration -  0 Input list =  [19, 1, 11, 13, 5, 6, 9]
+##Iteration -  1 Input list =  [1, 11, 13, 5, 6, 9, 19]
+##Iteration -  2 Input list =  [1, 11, 5, 6, 9, 13, 19]
+##Iteration -  3 Input list =  [1, 5, 6, 9, 11, 13, 19]
+##Iteration -  4 Input list =  [1, 5, 6, 9, 11, 13, 19]
+##Iteration -  5 Input list =  [1, 5, 6, 9, 11, 13, 19]
+##Elements after sorting:  [1, 5, 6, 9, 11, 13, 19]
+
+##Output of Quick Sort
+##[19, 1, 11, 13, 5, 6, 9]
+##[1, 5, 6, 9, 19, 11, 13]
+##[1, 5, 6, 9, 19, 11, 13]
+##[1, 5, 6, 9, 19, 11, 13]
+##[1, 5, 6, 9, 19, 11, 13]
+##[1, 5, 6, 9, 19, 11, 13]
+##[1, 5, 6, 9, 19, 11, 13]
+##[1, 5, 6, 9, 11, 13, 19]
+##[1, 5, 6, 9, 11, 13, 19]
 ##Elements after sorting:  [1, 5, 6, 9, 11, 13, 19]
